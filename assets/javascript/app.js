17e63cda2dbdbd2f17506bae2661bbe876c0bbc2
Shawn Feiz <shawnfeiz@gmail.com>
2017-12-27 21:50:12 -0600

    Minor improvements.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,29 +2,32 @@ $(document).ready(function() {
 
 //array of strings that are each related to the topic: Food? Sports Teams? Date night destinations? 
 var topics = ["tacos", "ice cream", "burger", "steak", "sandwich", "hot dogs", "lasagna", "pasta"];
+var img;
 
 //create a for loop that creates buttons for each string in the topics array above
-    for (var i = 0; i < topics.length; i++) {
-        var newButton = $("<button>");
-        newButton.text(topics[i]).addClass("btn-lg btn-primary btn-custom").attr("data-person", topics[i]);
-        $(".buttons").append(newButton);
-        console.log(newButton);
-    };
-
-function userButton() {
-    var newFood = $(".food-input");
-    topics.push(newFood);
+for (var i = 0; i < topics.length; i++) {
     var newButton = $("<button>");
-    newButton.text(newFood).addClass("btn-lg btn-primary btn-custom").attr("data-person", newFood);
+    newButton.text(topics[i]).addClass("btn-lg btn-primary btn-custom").attr("data-person", topics[i]);
     $(".buttons").append(newButton);
-    console.log(topics);
+    console.log(newButton);
 };
 
+
+    // function userButton() {
+    //     var newFood = $(".food-input").val();
+    //     topics.push(newFood);
+    //     // var newButton1 = $("<button>");
+    //     // newButton1.text(newFood).addClass("btn-lg btn-primary btn-custom").attr("data-person", newFood);
+    //     // $(".buttons").append(newButton1);
+    //     console.log(topics);
+    // };
+
+
 //===================================================================
 //===================================================================
 
 
-    $(".btn-custom").on("click", function() {
+    $("button").on("click", function() {
         //variable that holds the giphy url for AJAX call
         var food = $(this).attr("data-person");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
@@ -34,31 +37,54 @@ function userButton() {
             url: queryURL,
             method: "GET"           
         }).done(function(response){
-
-        console.log(response);
-        var results = response.data;
-        var foodDiv = $("<div>");
+            var results = response.data;
+            var foodDiv = $("<div>");
         
         // create a for loop that creates a div with img attribuite for each of the ten results
         for (var i = 0; i < results.length; i++) {
-            var img = $("<img>");  
-            img.attr("src", results[i].images.fixed_height.url); 
+            img = $("<img>");    
+            img.attr("data-state", "still").addClass("gif"); 
+            img.attr("src", results[i].images.fixed_height_still.url); 
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+            foodDiv.append(p);
             foodDiv.append(img);
             $(".appendImg").prepend(foodDiv);
-
         };
-    
+        
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            img.attr("src", results[i].images.fixed_height.url);
+            img.attr("data-state", "animate");
+            } else {
+            img.attr("src", results[i].images.fixed_height_still.url);
+            img.attr("data-state", "still");
+            };
 
+        
 
 
         });
     });
 
-$(".submitButton").on("click", function(){
-    userButton();
-});
+    // $(".gif").on("click", function(){
+    //     console.log("hello");
+    //     var state = $(this).attr("data-state");
+    //         if (state === "still") {
+    //             img.attr("src", results[i].images.fixed_height.url);
+    //             img.attr("data-state", "animate");
+    //           } else {
+    //             img.attr("src", results[i].images.fixed_height_still.url);
+    //             img.attr("data-state", "still");
+    //           };
 
+    // });
 
+// $(".submitButton").on("click", function(){
+//     userButton();
+// });
+
+console.log(img);
 
 });
 
